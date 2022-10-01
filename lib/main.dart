3012148f80603e4b14992d2fb48fd95fc2bf8681
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-10-01 12:54:55 +0500

    travel project setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          
+        ),
+      ),
+    );
+}
+}
